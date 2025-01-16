1cb67efc1d8bf70e25386d4625008f90674f6fc0
Terry Phillips <terrydphillips@gmail.com>
2025-01-15 19:23:35 -0500

    Update to 2025 apriltag layout

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -63,7 +63,7 @@ public class SwerveSubsystem extends SubsystemBase
   /**
    * AprilTag field layout.
    */
-  private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
+  private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
