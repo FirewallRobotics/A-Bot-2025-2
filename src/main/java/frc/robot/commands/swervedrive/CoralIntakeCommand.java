d47dc5647084cd308d3258cec36b45fdad6d661c
Cpixel151 <pixeldigit151@gmail.com>
2025-01-20 18:29:17 -0500

    Added shoot and intake. Check isfinished later.

---
+++ src/main/java/frc/robot/commands/swervedrive/CoralIntakeCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.swervedrive;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralHoldSubsystem;
+
+public class CoralIntakeCommand extends Command {
+  public final CoralHoldSubsystem m_Coral;
+
+  public CoralIntakeCommand(CoralHoldSubsystem c_Subsystem) {
+    m_Coral = c_Subsystem;
+  }
+
+  // Called when the command is first scheduled
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_Coral.intake();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_Coral.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    //Should come back to this
+    return false;
+  }
+}
