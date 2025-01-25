ecd6552546b3436107edee1432b971a1d89b0548
ItsYour14 <jzfortnite987@gmail.com>
2025-01-25 13:25:08 -0500

    elevator subsystem 😁

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,20 +1,47 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj.Encoder;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
-  private final SparkFlex motor;
-  private final Encoder encoder;
+  private final SparkFlex leftMotor;
+  private final SparkFlex rightMotor;
+  private final SparkFlexConfig leftMotorConfig;
+  private final SparkFlexConfig rightMotorConfig;
+  private RelativeEncoder encoder;
+  private SparkClosedLoopController closedLoopController;
 
   // Elevator levels in encoder ticks
-  private final int[] levels = {0, 1000, 2000, 3000, 4000};
+  private final double[] levels = {0, 1000, 2000, 3000, 4000};
 
   public ElevatorSubsystem() {
-    motor = new SparkFlex(0, MotorType.kBrushless); // Assign motor controller port
-    encoder = new Encoder(0, 1); // Assign encoder ports
+    leftMotor = new SparkFlex(9, MotorType.kBrushless); // Assign motor controller port
+    rightMotor = new SparkFlex(10, MotorType.kBrushless); // Assign motor controller port
+    closedLoopController = leftMotor.getClosedLoopController();
+    encoder = leftMotor.getEncoder();
+    leftMotorConfig = new SparkFlexConfig();
+    rightMotorConfig = new SparkFlexConfig();
+    leftMotorConfig.encoder.positionConversionFactor(1);
+    leftMotorConfig.encoder.velocityConversionFactor(1);
+    leftMotorConfig.smartCurrentLimit(50);
+    rightMotorConfig.smartCurrentLimit(50);
+    leftMotorConfig.idleMode(IdleMode.kBrake);
+    rightMotorConfig.idleMode(IdleMode.kBrake);
+    rightMotorConfig.inverted(true);
+    rightMotorConfig.follow(leftMotor);
+    leftMotor.configure(
+        leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightMotor.configure(
+        rightMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public void setLevel(int level) {
@@ -25,26 +52,16 @@ public class ElevatorSubsystem extends SubsystemBase {
     moveToPosition(levels[level]);
   }
 
-  private void moveToPosition(int position) {
-    double speed = calculateSpeed(position);
-    motor.set(speed);
-  }
-
-  private double calculateSpeed(int targetPosition) {
-    int currentPosition = encoder.get();
-    int error = targetPosition - currentPosition;
-
-    // Simple proportional control for example purposes
-    double kP = 0.01;
-    return kP * error;
+  private void moveToPosition(double position) {
+    closedLoopController.setReference(position, ControlType.kPosition, ClosedLoopSlot.kSlot0);
   }
 
   public void stop() {
-    motor.set(0);
+    leftMotor.set(0);
   }
 
   public boolean isFinished(int position) {
-    if (levels[position] - encoder.get() == 0) {
+    if (levels[position] - (leftMotor.getEncoder().getPosition()) == 0) {
       return true;
     } else {
       return false;
