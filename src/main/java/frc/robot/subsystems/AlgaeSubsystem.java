a143849531c74900fe26983939fc1d80f2a9d9be
Leo Maturino <Leo.maturino@gmail.com>
2025-02-01 15:44:50 -0500

    add algae branch

--- src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
@@ -1,88 +1,57 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-
+import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-
-import edu.wpi.first.wpilibj2.command.Command;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
 public class AlgaeSubsystem extends SubsystemBase {
-private final SparkMax m_highAlgaeMotor, m_lowAlgaeMotor;
-  /** Creates a new AlgaeSubsystem. */
+
+  private final SparkFlex motor;
+  private final SparkFlexConfig motorConfig;
+
+  // private final Encoder encoder;
+
   public AlgaeSubsystem() {
-    // motors
-    m_highAlgaeMotor = new SparkMax(AlgaeConstants.kHighAlgaeCANId, MotorType.kBrushless);
-    m_lowAlgaeMotor = new SparkMax(AlgaeConstants.kLowAlgaeCANId, MotorType.kBrushless);
+    motor = new SparkFlex(2, MotorType.kBrushless);
+    motorConfig = new SparkFlexConfig();
 
-    m_highAlgaeMotor.configure(Configs.AlgaeSubsystem.highAlgaeMotorConfig, ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
-    m_lowAlgaeMotor.configure(Configs.AlgaeSubsystem.lowAlgaeMotorConfig, ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
-
+    // encoder = new Encoder(1, 1);
   }
 
+  /**
+   * This method returns a constant speed for now.
+   * Adjust this to take joystick input or PID control later on.
+   */
+  private double setSpeed() {
+    double kP = 0.1; // Tweak as necessary.
+    return kP * 1;  // Currently a placeholder for speed calculation
+  }
+
+  // This method will be called in the periodic loop for constant operation if needed
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-     //SmartDashboard.putNumber("Coral_Left RPM", m_leftCoralEncoder.getVelocity());
-   // SmartDashboard.putNumber("Coral_Right RPM", m_rightCoralEncoder.getVelocity());
+    // Add any periodic tasks here, such as motor feedback or sensor checks.
   }
 
-  public void ShootAlgaeOut(){
-    m_highAlgaeMotor.set(AlgaeConstants.kAlgaeSpeedOut);
-    m_lowAlgaeMotor.set(-AlgaeConstants.kAlgaeSpeedOut);
+  // Shoots the ball (or algae). The claw motor runs in the forward direction.
+  public void shoot() {
+    motorConfig.inverted(false);
+    motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motor.set(setSpeed());  // Run motor forward to shoot the ball (algae)
   }
 
-  public void FeedAlgaeIn(){
-    m_highAlgaeMotor.set(-AlgaeConstants.kAlgaeSpeedIn);
-    m_lowAlgaeMotor.set(AlgaeConstants.kAlgaeSpeedIn);
+  // Intakes the ball (algae). The claw motor runs in the reverse direction.
+  public void intake() {
+    motorConfig.inverted(true);  // Invert motor direction for intake
+    motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motor.set(setSpeed());  // Reverse the motor to pull the algae into the claw
   }
 
-  public void AlgaeStop() {
-    m_highAlgaeMotor.set(0);
-    m_lowAlgaeMotor.set(0);
+  // Stops the motor. Useful for stopping the claw at any time.
+  public void stop() {
+    motor.set(0);  // Completely stop motor when the claw doesn't need to run
   }
-
-  // same as ShootCoralOut() except is a Command class
-  public Command ShootAlgaeOutCmd() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-                m_highAlgaeMotor.set(AlgaeConstants.kAlgaeSpeedOut);
-                m_lowAlgaeMotor.set(-AlgaeConstants.kAlgaeSpeedOut);
-              }
-    );
-  }
-  // same as shooterFeedNoteIn() except it is a Command class
-   public Command FeedAlgaeInCmd() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-                m_highAlgaeMotor.set(-AlgaeConstants.kAlgaeSpeedIn);
-                m_lowAlgaeMotor.set(AlgaeConstants.kAlgaeSpeedIn);
-              }
-    );
-  }
-
-  // same as shooterStop() except it is a Command class
-  public Command AlgaeStopCmd() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-                m_highAlgaeMotor.set(0);
-                m_lowAlgaeMotor.set(0);
-              }
-    );
-  }
-  }
+}
