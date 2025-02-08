2d36390f6b4ec64ac19816e3b34b67de6bc68b94
ItsYour14 <jzfortnite987@gmail.com>
2025-02-08 16:32:03 -0500

    Arm Commands

--- src/main/java/frc/robot/commands/ArmLower.java
+++ src/main/java/frc/robot/commands/ArmLower.java
@@ -1,15 +1,14 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimberSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
 
+public class ArmLower extends Command {
+  private ClimberSubsystem m_subsystem;
 
-public class ArmLower {
-    private ClimberSubsystem m_subsystem;
-
-    public ArmLower(ClimberSubsystem subsystem) {
-        m_subsystem = subsystem;
-        // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(subsystem);
-    }
+  public ArmLower(ClimberSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
 }
--- src/main/java/frc/robot/commands/ArmRaise.java
+++ src/main/java/frc/robot/commands/ArmRaise.java
@@ -1,15 +1,14 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimberSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
 
+public class ArmRaise extends Command {
+  private ClimberSubsystem m_subsystem;
 
-public class ArmRaise {
-    private ClimberSubsystem m_subsystem;
-
-    public ArmRaise(ClimberSubsystem subsystem) {
-        m_subsystem = subsystem;
-        // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(subsystem);
-    }
+  public ArmRaise(ClimberSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
 }
