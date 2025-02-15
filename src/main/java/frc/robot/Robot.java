5d629473e43cd06453f246d97a9ee42c0e6137ec
Leo Maturino <Leo.maturino@gmail.com>
2025-02-15 15:27:52 -0500

    fix spotless

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.pathfinding.Pathfinding;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
