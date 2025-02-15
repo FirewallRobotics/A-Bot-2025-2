5aa67b9f8778ab1464cf5275400ee1c19c339f0f
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2025-02-15 16:12:23 -0500

    spotless

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,6 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -39,7 +38,6 @@ import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import java.io.File;
 import java.util.Optional;
 import swervelib.SwerveInputStream;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -115,7 +113,11 @@ public class RobotContainer {
     new EventTrigger("DropCoral").onTrue(new CoralShootCommand(coralHoldSubsystem));
     new EventTrigger("ElevatorLvl1").onTrue(new ElevatorMoveLevel1(elevatorSubsystem));
     new EventTrigger("GrabCoral")
-        .onTrue(new SequentialCommandGroup(new ElevatorMoveLevel1(elevatorSubsystem), new WaitCommand(1), new CoralIntakeCommand(coralHoldSubsystem)));
+        .onTrue(
+            new SequentialCommandGroup(
+                new ElevatorMoveLevel1(elevatorSubsystem),
+                new WaitCommand(1),
+                new CoralIntakeCommand(coralHoldSubsystem)));
   }
 
   public void init() {
