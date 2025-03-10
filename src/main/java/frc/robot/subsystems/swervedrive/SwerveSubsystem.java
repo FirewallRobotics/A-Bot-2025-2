7378504168ce909ba7a6a75b8abc1d2768933c4f
NoobinOut <110687587+NoobinOut@users.noreply.github.com>
2025-03-10 18:53:39 -0400

    changed the navgrid and fixed the slow goto movements

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -232,7 +232,7 @@ public class SwerveSubsystem extends SubsystemBase {
     // Create the constraints to use while pathfinding
     PathConstraints constraints =
         new PathConstraints(
-            1, 4.0, swerveDrive.getMaximumChassisAngularVelocity(), Units.degreesToRadians(720));
+            5, 4.0, swerveDrive.getMaximumChassisAngularVelocity(), Units.degreesToRadians(720));
 
     // Since AutoBuilder is configured, we can use it to build pathfinding commands
     return AutoBuilder.pathfindToPose(
