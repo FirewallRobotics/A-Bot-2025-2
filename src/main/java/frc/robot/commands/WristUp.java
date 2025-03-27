d0abe2e2bf9d1070bb5a3e612a61404d8e827cbf
Cpixel151 <milostories151@gmail.com>
2025-03-26 20:18:34 -0400

    Spotless

--- src/main/java/frc/robot/commands/WristUp.java
+++ src/main/java/frc/robot/commands/WristUp.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CoralHoldAngleSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
